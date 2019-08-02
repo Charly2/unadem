ea5f3075fea7401a463934ea151ebb6c5287683c
Charly2 <char2296@hotmail.com>
2019-08-01 21:00:18 -0500

    aa

--- resources/views/excel.blade.php
+++ resources/views/excel.blade.php
@@ -3,12 +3,20 @@
         background-color: #5cb85c;
 
     }
+    .naranja{
+        background-color: #b87b00;
+
+    }
+    .azul{
+        background-color: #00a6e2;
+
+    }
     .center{
         text-align: center;
     }
 
     tr td {
-
+        border: 1px solid red;
     }
 
     tr > td {
@@ -26,21 +34,50 @@
         <td class="verde"></td>
         <td class="verde"></td>
         <td class="verde"></td>
-        <td>Planeación Didáctica</td>
+        @foreach($criterios as $c )
+            <td class="azul" colspan="{{sizeof($c['items'])}}">{{$c['titulo']}}</td>
+        @endforeach
+
+        <td class="naranja"></td>
+        <td class="naranja"></td>
+        <td class="naranja"></td>
     </tr>
     <tr>
         <td class="verde">Folio</td>
         <td class="verde">Apellido Paterno</td>
         <td class="verde">Apellido Materno</td>
         <td class="verde">Nombre(s)</td>
+        @foreach($criterios as $c )
+            @foreach($c['items'] as $cc )
+                <td class="azul" >{{$cc}}</td>
+            @endforeach
+        @endforeach
+        <td class="naranja">Observaciones generales</td>
+        <td class="naranja">Comentarios</td>
+        <td class="naranja">Calificación final</td>
     </tr>
     @foreach($docente as $d)
     <tr>
-        <td>{{$d->folio}}</td>
-        <td>{{$d->apPaterno}}</td>
-        <td>{{$d->apMaterno}}</td>
-        <td>{{$d->nombres}}</td>
+        <td>{{$d['evalucion']->folio}}</td>
+        <td>{{$d['evalucion']->apPaterno}}</td>
+        <td>{{$d['evalucion']->apMaterno}}</td>
+        <td>{{$d['evalucion']->nombres}}</td>
 
+        @foreach($d['calificaciones'] as $dd)
+            <td>{{$dd}}</td>
+        @endforeach
+
+        <td>
+        @foreach($d['obs'] as $dd)
+            {{$dd->observacion}}
+        @endforeach
+        </td>
+        <td>
+            {{$d['evalucion']->comentario}}
+        </td>
+        <td>
+            {{$d['evalucion']->calificacion}}
+        </td>
     </tr>
     @endforeach
 </table>
--- routes/web.php
+++ routes/web.php
@@ -16,32 +16,91 @@
 Route::get('/', function () {
 
 
-    $data=  DB::table('usuario')->get()->toArray();
+    //$data=  DB::table('usuario')->get()->toArray();
 
 
 
 
 
-    //return view('welcome');
+    return view('welcome');
 });
 
 
 
 Route::get('/prueba',function (){
-    $header_item[] = array('Usuario', 'Password');
+
     $items = DB::table('docente')->take(30)->get();
-    /*foreach($items as $item) {
-        $header_item[] = array(
-            'Usuario' => $item->usuario,
-            'Password' => $item->password,
-        );
-    }*/
-    Excel::create('Summary', function($excel) use ($items) {
+    $docente  = DB::select('SELECT * FROM `evaluacionDocente` e inner join docente d on e.docente = d.folio where e.evaluacion = 1 and e.periodo = "2018-2S-B2" and e.fechaTermino is not null');
+    $data  = [];
+    foreach ($docente as $d) {
+        $aux = [];
+        $aux['evalucion'] = $d;
+        $indicadorcali = DB::table('indicadorCalificacion')->where('idEvaluacion', $d->idEvaluacionDocente)->orderBy('indicador', 'ASC')->get();
+        foreach ($indicadorcali as $i ){
+            $aux['calificaciones'][] = $i->calificacion;
+        }
+        $obs=  DB::select('select * from evaluacionObservacion e inner join observaciones o on  o.idObservacion = e.observacion where e.evaluacion = "'.$d->idEvaluacionDocente.'" ');
+        $aux['obs'] = $obs;
+        $data[] = $aux;
+    }
+
+    $criterios = DB::table('criterio')->where('idEvaluacion',1)->get();
+    $header = [];
+    foreach ($criterios as $c){
+        $aux = [];
+        $aux['titulo'] = $c->nombre;
+        $indi = DB::table('indicador')->where('idCriterio',$c->idCriterio)->get();
+        foreach ($indi as $i ){
+            $aux['items'][] = $i->titulo;
+        }
+        $header[]= $aux;
+    }
+    Excel::create('Summary', function($excel) use ($data,$header) {
         $excel->setTitle('Summary');
-        $excel->sheet('Summary', function($sheet) use ($items) {
+        $excel->sheet('Summary', function($sheet) use ($data,$header) {
             //$sheet->fromArray($header_item, null, 'A1', false, false);
-            $sheet->loadView('excel',['docente'=>$items]);
+            $sheet->loadView('excel',['docente'=>$data,'criterios'=>$header]);
         });
     })->download('xlsx');
 });
 
+
+
+Route::get('/tabla',function (){
+
+
+
+    $items = DB::table('docente')->take(30)->get();
+    $docente  = DB::select('SELECT * FROM `evaluacionDocente` e inner join docente d on e.docente = d.folio where e.evaluacion = 1 and e.periodo = "2018-2S-B2" and e.fechaTermino is not null');
+    $data  = [];
+    foreach ($docente as $d) {
+        $aux = [];
+        $aux['evalucion'] = $d;
+        $indicadorcali = DB::table('indicadorCalificacion')->where('idEvaluacion', $d->idEvaluacionDocente)->orderBy('indicador', 'ASC')->get();
+        foreach ($indicadorcali as $i ){
+            $aux['calificaciones'][] = $i->calificacion;
+        }
+        $obs=  DB::select('select * from evaluacionObservacion e inner join observaciones o on  o.idObservacion = e.observacion where e.evaluacion = "'.$d->idEvaluacionDocente.'" ');
+        $aux['obs'] = $obs;
+        $data[] = $aux;
+    }
+
+    $criterios = DB::table('criterio')->where('idEvaluacion',1)->get();
+    $header = [];
+    foreach ($criterios as $c){
+        $aux = [];
+        $aux['titulo'] = $c->nombre;
+        $indi = DB::table('indicador')->where('idCriterio',$c->idCriterio)->get();
+        foreach ($indi as $i ){
+            $aux['items'][] = $i->titulo;
+        }
+        $header[]= $aux;
+    }
+
+
+
+
+
+
+    return view('excel',['docente'=>$data,'criterios'=>$header]);
+});
